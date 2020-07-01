05fda2b93eb2565f1811dc9d2971d3c1231a41c0
Bennici <alexandre-ben@hotmail.fr>
2020-07-01 09:43:12 +0200

    add new function to app

--- app.R
+++ app.R
@@ -3,27 +3,34 @@
 library("ows4R")
 library("sp")
 library("shiny")
+library("DT")
 ########################
 ui <- fluidPage(
     h3("Parsed query string"),
     verbatimTextOutput("queryText"),
-    tableOutput('table')
+    verbatimTextOutput("value"),
+    DTOutput('table')
+    
+    #tableOutput("table")
 )
 
 server <- function(input, output, session) {
     resume.table<-data.frame(NULL)
-    output$queryText <- renderText({
-        query <- parseQueryString(session$clientData$url_search)
-        paste(sep = "",
-              "pid: ", query$pid, "\n",
-              "layer: ", query$layer, "\n",
-              "wfs_server: ",query$wfs_server, "\n",
-              "wfs_version: ", query$wfs_version, "\n",
-              "strategy: ", query$strategy, "\n",
-              "par: ", query$par, "\n")
-    })
+
     observe({
+        
         query <- parseQueryString(session$clientData$url_search)
+        
+        output$queryText <- renderText({
+            #query <- parseQueryString(session$clientData$url_search)
+            paste(sep = "",
+                  "pid: ", query$pid, "\n",
+                  "layer: ", query$layer, "\n",
+                  "wfs_server: ",query$wfs_server, "\n",
+                  "wfs_version: ", query$wfs_version, "\n",
+                  "strategy: ", query$strategy, "\n",
+                  "par: ", query$par, "\n")
+        })
         
         pid <- if (!is.null(query$pid)){
             as.character(query$pid)
@@ -55,21 +62,21 @@ server <- function(input, output, session) {
         }
         
         par <-if (!is.null(query$par)){
-            as.character(query$par)
-        }else{
-            NULL
-        }
+             as.character(query$par)
+         }else{
+             NULL
+         }
         
         if(!is.null(pid)&!is.null(layer)&!is.null(wfs_server)&!is.null(wfs_version)&!is.null(strategy)&!is.null(par)){
             
-            # #Connect to OGC CSW Catalogue to get METADATA
-            CSW <- CSWClient$new(
-                url = "https://geonetwork-sdi-lab.d4science.org/geonetwork/srv/eng/csw",
-                serviceVersion = "2.0.2",
-                logger = "INFO"
-            )
-            #Get metadata for dataset
-            md <- CSW$getRecordById(pid, outputSchema = "http://www.isotc211.org/2005/gmd")
+            # # #Connect to OGC CSW Catalogue to get METADATA
+            # CSW <- CSWClient$new(
+            #     url = "https://geonetwork-sdi-lab.d4science.org/geonetwork/srv/eng/csw",
+            #     serviceVersion = "2.0.2",
+            #     logger = "INFO"
+            # )
+            # #Get metadata for dataset
+            # md <- CSW$getRecordById(pid, outputSchema = "http://www.isotc211.org/2005/gmd")
             
             #Connect to OGC WFS to get DATA
             WFS <- WFSClient$new(
@@ -80,18 +87,23 @@ server <- function(input, output, session) {
             #Get feature type for dataset
             ft <- WFS$capabilities$findFeatureTypeByName(layer)
             #Get data features for dataset
+            if(is.null(par)){
+            data.sf <- ft$getFeatures()
+            }else{ 
             data.sf <- switch(strategy,
-                              "ogc_filter"=ft$getFeatures(if(!is.null(par)){cql_filter = gsub(" ", "%20", gsub("''", "%27%27", URLencode(par)))}),
-                              "ogc_viewparams"=ft$getFeatures(if(!is.null(par)){viewparams = URLencode(par)})
+                              "ogc_filters"=ft$getFeatures(cql_filter = gsub(" ", "%20", gsub("''", "%27%27", URLencode(par)))),
+                              "ogc_viewparams"=ft$getFeatures(viewparams = URLencode(par))
             )
-            data.sp <- as(data.sf, "Spatial")
-            resume.table<<-as.data.frame(head(data.sp))
+            }
+            #data.sp <- as(data.sf, "Spatial")
+            #resume.table<<-as.data.frame(head(data.sp))
+            output$table <- renderDT(as.data.frame(data.sf), options = list(lengthChange = FALSE))
             
+            #output$table <- renderTable({as.data.frame(data.sf)})
+            output$value <- renderText({paste(sep = "","number of values : ",nrow(data.sf))})
         }else{}
-    })
-    
-    if(exists("resume.table")){output$table <- renderTable({head(resume.table)})}else{}
-    
+        })
+        
 }
 
 shinyApp(ui, server)
