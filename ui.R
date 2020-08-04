918e58c7baccebb709f4e70023db579e9e8a2cec
Bennici <alexandre-ben@hotmail.fr>
2020-08-04 15:27:27 +0200

    Modularizing Shiny app code

---
+++ ui.R
@@ -0,0 +1,31 @@
+## ui.R ##
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("QueryInfo", tabName = "QueryInfo", icon = icon("dashboard")),
+    menuItem("Table", icon = icon("th"), tabName = "Table")
+  )
+)
+
+body <- dashboardBody(
+    tabItems(
+    tabItem(tabName = "QueryInfo",
+            h2("Dashboard tab content"),QueryInfoUI(id = "id_1")
+    ),
+
+    tabItem(tabName = "Table",
+            h2("Table"),DataTableUI(id = "id_2")
+    )
+  )
+)
+
+# Put them together into a dashboardPage
+dashboardPage(
+  dashboardHeader(title = "Sdilab Dashboard",tags$li(class = "dropdown",
+                                                     tags$a(href="https://www.blue-cloud.org", target="_blank", 
+                                                            tags$img(height = "30px", alt="SNAP Logo", src="https://www.blue-cloud.org/sites/all/themes/arcadia/logo.png")
+                                                     )
+  )),
+  sidebar,
+  body
+)
+
